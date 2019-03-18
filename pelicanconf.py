805fabdaa3187733b8792b3f30e1a75caed688ea
naireeni <naireen.imtiaz@gmail.com>
2019-03-18 11:53:15 +0000

    adding blog

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ PATH = 'content'
 TIMEZONE = 'Europe/London'
 
 DEFAULT_LANG = 'en'
-
+IGNORE_FILES = ['.ipynb_checkpoints']
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
